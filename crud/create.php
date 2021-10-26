b544847a849ee3f779d9374145e24cc0a79e8a60
hotequil <hotequil@protonmail.com>
2021-10-25 21:04:18 -0300

    feito create

--- crud/create.php
+++ crud/create.php
@@ -1,23 +1,22 @@
 <?php
     header("Content-type: text/html; charset=ISO-88591");
-
+    
     $server = "localhost";
     $user = "root";
     $password = "";
-    $database = "subject";
+    $database = "graduation";
+    $table = "subject";
     $name = $_POST['name'];
     $course = $_POST['course'];
     $semester = $_POST['semester'];
     $workload = $_POST['workload'];
-    $connection = mysqli_connect($server, $user, $password, $database);
-
-    mysqli_set_charset($connection, 'utf8');
-
-    if(!$connection) die("error in connection".mysqli_connect_error());
-
-    echo $consult = "INSERT INTO $database(name, course, semester, workload) VALUES ('$name', '$course', '$semester', '$workload')";
-
-    $result = mysqli_query($connection, $consult) or die("error in consult".mysqli_connect_error());
+    $connection = mysqli_connect($server, $user, $password, $database) or die("error in connection: ".mysqli_connect_error());
+	
+    mysqli_query($connection, 'SET character_set_results=utf8');
+    
+    echo $consult = "INSERT INTO $table(name, course, semester, workload) VALUES ('$name', '$course', '$semester', '$workload')";
+    
+    $result = mysqli_query($connection, $consult) or die("error in consult: ".mysqli_connect_error());
 
     echo "data salved in database";
 ?>
